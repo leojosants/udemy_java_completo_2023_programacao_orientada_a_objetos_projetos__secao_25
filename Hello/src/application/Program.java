c6fdd2cf8a3698e8e86002e9ebbbc96ff941f4cc
Leonardo Santos <leonardojosantos@gmail.com>
2023-12-29 12:01:25 -0300

    Creating a new JavaFX project in Eclipse and testing FXML

---
+++ Hello/src/application/Program.java
@@ -0,0 +1,11 @@
+/*-------------------- packages --------------------*/
+package application;
+
+/*-------------------- class Program --------------------*/
+public class Program {
+
+	/*-------------------- main method --------------------*/
+	public static void main(String[] args) {
+		System.out.println("Olá, Mundo!");
+	}
+}
