55a5156590f0062ed6ce037574f738ff3d7a4fdf
Leonardo Santos <leonardojosantos@gmail.com>
2023-12-29 15:10:16 -0300

    Local project created

---
+++ workshop-javafx-jdbc/src/application/Main.java
@@ -0,0 +1,32 @@
+/*-------------------- packages --------------------*/
+package application;
+	
+/*-------------------- imports --------------------*/
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+/*-------------------- class Alerts --------------------*/
+public class Main extends Application {
+
+	/*-------------------- functions --------------------*/
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} 
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/*-------------------- main method --------------------*/
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
